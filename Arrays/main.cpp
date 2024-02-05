da3f47822a72536260d101784dbc5ea6a6c8e80c
Nazarets <ksenianazarec995@mail.com>
2024-02-05 23:18:27 +0300

    introduction to erase

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -3,5 +3,19 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	const int SIZE = 5;
+	int arr[SIZE];
+	// input elements from kbrd
+	for (int j = 0; j < SIZE; j++)
+	{
+		int c;
+		cin >> c;
+		arr[j] = c;
+	}
+	// output the list on the screen
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 }
